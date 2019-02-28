13fa0ff7191e834aa6bb4d1c47f8071c1e54b77a
naviiiiin5 <47624696+naviiiiin5@users.noreply.github.com>
2019-02-28 18:03:52 +0530

    Update mainGame.ts

--- mainGame.ts
+++ mainGame.ts
@@ -1,6 +1,6 @@
 /// <reference path="Paddle.ts" />
 /// <reference path="pixi.js.d.ts" />
-
+/// <reference path="pingPongGameController.ts" />
 namespace IGpingpong {
-    export let gc=new gameController();
+    export let gc=new pingpong();
 }
