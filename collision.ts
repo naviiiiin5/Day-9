467a8b3814541a98b9b9beee5d69ebf403a911fa
naviiiiin5 <47624696+naviiiiin5@users.noreply.github.com>
2019-03-01 18:25:17 +0530

    Update collision.ts

--- collision.ts
+++ collision.ts
@@ -1,10 +1,13 @@
 namespace IGpingpong{
     export class Collider{
-        check_collision(body1:iRigidBody,body2:iRigidBody,handler:any){
-            if(!((body1.x>body2.x+body2.height) || (body1.x+body1.height<body2.y)||(body1.x+body1.width<body2.x)||(body1.y+body1.height<body2.y)))
+        check_collision(body1:iRigidBody,body2:iRigidBody){
+            if(!(( body1.y + body1.height ) <(body2.y)) ||
+		    (body1.y > ( body2.y + body2.height)) ||
+		    ((body1.x + body1.width ) < body2.x ) ||
+		    (body1.x > (body2.x + body2.width )))
             {
-                handler();
-            }
+                return true;  
         }
     }
+}
 }
