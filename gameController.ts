94b120209a61932c96aa5b0212cc7056fa94e822
naviiiiin5 <47624696+naviiiiin5@users.noreply.github.com>
2019-03-05 11:24:10 +0530

    Update gameController.ts

--- gameController.ts
+++ gameController.ts
@@ -2,7 +2,7 @@ namespace IGpingpong{
    export class gameController{
       public app!: PIXI.Application;
       constructor(){
-         this.app=new PIXI.Application(800,800);
+         this.app=new PIXI.Application({width:800,height:800,backgroundColor: 0xffffff});
          document.body.appendChild(this.app.view);
          this.start();
          this.app.ticker.add(this.update.bind(this));
